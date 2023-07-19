70562e4162b34550e7b4eb8180123d6663636f26
Vijayakanth M <vijayakanth@bicsglobal.com>
2023-07-19 18:42:44 +0530

    environment updated

--- stackbit.config.js
+++ stackbit.config.js
@@ -8,7 +8,7 @@ export default {
   contentSources: [
     new ContentfulContentSource({
       spaceId: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-      environment: process.env.CONTENTFUL_ENVIRONMENT || "master",
+      environment: process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT || "master",
       previewToken: process.env.NEXT_PUBLIC_CONTENTFUL_PREVIEW_TOKEN,
       deliveryToken: process.env.NEXT_PUBLIC_CONTENTFUL_DELIVERY_TOKEN,
       accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_MANAGEMENT_TOKEN,
